346d4d9ad94f7bdcae03e15a733ae78dccdd356e
sabari20070602 <sabari20070602@gmail.com>
2025-09-15 12:39:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-
   event.preventDefault();
 
   let name = document.getElementById("name").value;
-
   let email = document.getElementById("email").value;
-
   let message = document.getElementById("message").value;
 
   document.getElementById("formResponse").innerText =
-
     "Thank you, " + name + "! Your message has been received.";
-
     
-
-  // Reset form fields
-
   document.getElementById("contactForm").reset();
-
 });
